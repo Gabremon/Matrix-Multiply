96e5af972281a55a6a9f09be5f2298bc729d94ae
Gabremon <gwomack2003@gmail.com>
2024-03-04 09:37:52 -0800

    basic commit

--- src/MatrixMultiply.java
+++ src/MatrixMultiply.java
@@ -1,3 +1,35 @@
-public class MatrixMultiply(int[] A, int[] B) {
+public class MatrixMultiply {
+    public static int[][] matrixMultiply(int[][] A, int[][] B) {
+        if (A[0].length != B.length) {
+            throw new IllegalArgumentException("Incompatible dimensions");
+        }
 
+        int[][] C = new int[A.length][B[0].length];
+
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < B[0].length; j++) {
+                C[i][j] = 0;
+                for (int k = 0; k < A[0].length; k++) {
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+
+        return C;
+    }
+
+    public static void main(String[] args) {
+        int[][] A = {{1, 2, 3}, {4, 5, 6}};
+        int[][] B = {{7, 8}, {9, 10}, {11, 12}};
+
+        int[][] result = matrixMultiply(A, B);
+
+        // Print the result matrix
+        for (int[] row : result) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
 }
